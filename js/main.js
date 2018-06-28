907cff2e1b5b41cc8a4889cf4588d7d2861598c6
Mason Wu <masonwu8462@yahoo.com>
2018-06-28 13:59:22 -0400

    Fix: Revert addition of Mapbox API Key
    This was done in order to use the Google Maps API instead

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoibmVvY3l0ZSIsImEiOiJjaml3NmQwYWgzMTNyM3ZyZnVndmI4MTVvIn0.K4iUBS15kfiB3Vv_X7X5NA',
+    mapboxToken: '<your MAPBOX API KEY HERE>',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
