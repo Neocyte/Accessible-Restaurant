b3bf04623fc1a62a5601d59418feefdad11fc045
Mason Wu <masonwu8462@yahoo.com>
2018-07-06 19:48:07 -0400

    Fix: Consolidate service workers into one

--- js/service_worker.js
+++ js/service_worker.js
@@ -1,9 +1,12 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('restaurant-static-v1').then(function(cache) {
+    caches.open('main-static-v1').then(function(cache) {
       return cache.addAll([
         '/',
+        'index.html',
+        'restaurant_info.html',
         'css/styles.css',
+        'js/main.js',
         'js/restaurant_info.js',
         'js/dbhelper.js',
         'data/restaurants.JSON',
